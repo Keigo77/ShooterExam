3bac94a442b805eec51918fbe443d54608d74bd1
Keigo77 <kake32059kig@gmail.com>
2025-09-21 00:41:07 +0900

    add: UI・エフェクトアセットの追加

---
+++ Assets/!_ShooterExam/Scripts/OutGame/AudioSingleton.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioSingleton : MonoBehaviour
+{
+    public static AudioSingleton Instance;
+    [SerializeField] private AudioClip[] _bgmClips;
+    private int _beforeSceneIndex;
+    // 添え字0がBGM用，1がSE用のAudioSource
+    private AudioSource[] _audioSources;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        _audioSources = this.GetComponents<AudioSource>();
+        SceneManager.sceneLoaded += ChangeBGM;
+    }
+
+    /// <summary>
+    /// シーン遷移する度に実行し，同じBGMを使うシーンの遷移のみ，BGMを連続させる．
+    /// </summary>
+    private void ChangeBGM(Scene scene, LoadSceneMode loadMode)
+    {
+        if (_bgmClips[scene.buildIndex] == null)
+        {
+            _audioSources[0].Stop();
+        }
+        else if (_audioSources[0].clip != _bgmClips[scene.buildIndex])
+        {
+            _audioSources[0].clip = _bgmClips[scene.buildIndex];
+            _audioSources[0].Play();
+        }
+    }
+    
+    private void OnDestroy()
+    {
+        // オブジェクトが破壊された際にイベントの購読を解除
+        SceneManager.sceneLoaded -= ChangeBGM;
+    }
+}
---
+++ Assets/EasyCustomVFXUI/Scripts/TransitionController/TransitionProgressController.cs
@@ -0,0 +1,70 @@
+﻿using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+/// <summary>
+/// Controlling parameters of material for transition image
+/// </summary>
+
+
+[RequireComponent(typeof(RawImage))]
+[ExecuteAlways]
+public class TransitionProgressController : MonoBehaviour
+{
+    [Range(0f, 1f)] public float Progress = 0f;
+    [SerializeField] private float _transitionDuration = 1.5f;
+
+    private RawImage rawImage;
+    private Material material;
+
+    void OnEnable()
+    {
+        rawImage = GetComponent<RawImage>();
+        material = rawImage.material;
+        SetProgressProperty();
+    }
+
+    void Update()
+    {
+        SetProgressProperty();
+    }
+
+    private void SetProgressProperty()
+    {
+        if (material.HasProperty("_Progress"))
+        {
+            material.SetFloat("_Progress", Progress);
+        }
+        else
+        {
+            Debug.LogWarning("The material does not have a 'Progress' property.");
+        }
+    }
+
+    public async UniTask FadeOut()
+    {
+        await DOTween.To(() => Progress, x => Progress = x,
+                0f, _transitionDuration)
+            .OnUpdate(SetProgressProperty);
+    }
+    
+    public async UniTask FadeIn()
+    {
+        await DOTween.To(() => Progress, x => Progress = x,
+                1f, _transitionDuration)
+            .OnUpdate(SetProgressProperty);
+    }
+    
+    /// <summary> For Preventing memory leak </summary>  
+    private void OnDestroy()
+    {
+        if (!material)
+        {
+            Destroy(material);
+            material = null;
+        }
+    }
+}
